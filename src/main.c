10a122df5664e61604dc31162ea1b84a6e313258
Maicon Junior <maiconjnsv@gmail.com>
2026-01-26 17:18:17 -0300

    recebendo inputs e alteração no txt

--- src/main.c
+++ src/main.c
@@ -1,9 +1,35 @@
 #include <stdio.h>
+#include <string.h>
 
-
-
-
-int main(void)
+int main(int argc, char *argv[])
 {
-    printf("first");
+    FILE *arquivo;
+    //Validação de comandos
+    if( argc < 2)
+    {
+        printf("Comando não reconhecido, use wstaion help para verificar comandos.\n");
+    }
+
+    if(strcmp(argv[1], "wstaion") == 0)
+    {
+        return 0;
+    }
+
+    else if(argc >= 3  && strcmp(argv[1], "add") == 0
+            && strcmp(argv[2], "path") == 0)
+    {
+        arquivo = fopen("path.txt", "a");
+        fprintf(arquivo, "teste\n");
+    }
+
+    else
+    {
+        printf("Comando não reconhecido, use wstaion help para verificar comandos.\n");
+    }
+
+    return 0;
 }
+
+
+
+
