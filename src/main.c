4427e8cfaabd5ed2eb2b33018dc28832e8c17042
Maicon <maiconjnsv@gmail.com>
2026-01-26 22:07:48 -0300

    lógica de adcionar path

--- src/main.c
+++ src/main.c
@@ -1,35 +1,53 @@
 #include <stdio.h>
 #include <string.h>
 
+void add_path(char *path);
+
+
 int main(int argc, char *argv[])
 {
-    FILE *arquivo;
-    //Validação de comandos
-    if( argc < 2)
+    
+    // Para aceitar: wstaion add path "caminho"
+    // argc deve ser 4
+    if(argc == 1)
     {
-        printf("Comando não reconhecido, use wstaion help para verificar comandos.\n");
+        printf("Comando não reconhecido, use wstation help para verificar comandos.\n");
+        return 1;
     }
 
-    if(strcmp(argv[1], "wstaion") == 0)
+    if(argc == 4 && strcmp(argv[1], "add") == 0 && strcmp(argv[2], "path") == 0)
     {
-        return 0;
+        add_path(argv[3]);
+    } 
+    else if (argc < 4 && strcmp(argv[1], "add") == 0) 
+    {
+        printf("Erro: Faltou o caminho.\n");
     }
-
-    else if(argc >= 3  && strcmp(argv[1], "add") == 0
-            && strcmp(argv[2], "path") == 0)
+    else 
     {
-        arquivo = fopen("path.txt", "a");
-        fprintf(arquivo, "teste\n");
-    }
-
-    else
-    {
-        printf("Comando não reconhecido, use wstaion help para verificar comandos.\n");
+        printf("Comando não reconhecido, use wstation help para verificar comandos.\n");
     }
 
     return 0;
 }
 
+void add_path(char *path)
+{
+    if (path == NULL || strlen(path) == 0)
+    {
+        printf("Erro: caminho inválido.\n");
+        return;
+    }
 
+    FILE *arquivo = fopen("path.txt", "a");
+    if (arquivo == NULL)
+    {
+        printf("Erro fatal: Não foi possível abrir ou criar 'path.txt'.\n");
+        return;
+    }
 
+    fprintf(arquivo, "%s\n", path);
+    fclose(arquivo);
 
+    printf("Caminho adicionado: %s\n", path);
+}
